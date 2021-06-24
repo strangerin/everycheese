047738c8ea91cea905b02b02c0df1756f065b2fb
kizyma <kizimayarik01@gmail.com>
2021-06-24 15:30:06 +0300

    Implemented cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from . import views 
 
 app_name = "cheeses" 
-urlpatterns = [ path( route='', view=views.CheeseListView.as_view(), name='list' ), ]
+urlpatterns = [ 
+    path(route='', view=views.CheeseListView.as_view(), name='list'), 
+    path(route='<slug:slug>/', view=views.CheeseDetailView.as_view(), name='detail')
+]
